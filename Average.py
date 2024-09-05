b033f1d207cb02f16f46d5c79190800e1b8f9507
Saifan594 <saifanhasan72@gmail.com>
2024-09-05 18:27:07 +0600

    skibidi

--- Average.py
+++ Average.py
@@ -1,1 +1,12 @@
-print("hello world")
+friend1=int(input("Enter friend1's amount: "))
+friend2=int(input("Enter friend2's amount: "))
+friend3=int(input("Enter friend3's amount: "))
+friend4=int(input("Enter friend4's amount: "))
+
+sum=friend1+friend2+friend3+friend4
+
+print(f"Total amount: {sum}")
+
+mean=sum/4
+
+print(f"Everyone should pay: {mean}")
